587b7edd96c08a844a381b3b4b628a38356eb7b8
Aayush Jain <f20180320@hyderabad.bits-pilani.ac.in>
2020-11-11 19:18:34 +0530

    make arrows sticky

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public int damage;
+    private Rigidbody rb;
+    private Transform transform;
+    void Awake(){
+        rb = gameObject.GetComponent<Rigidbody>();
+        transform = gameObject.GetComponent<Transform>();
+
+        Debug.Log("*********************arise");
+
+    }
+
+    void OnCollisionEnter(Collision col){
+        rb.isKinematic = true;
+        transform.parent = col.gameObject.transform;
+        Debug.Log("ouch");
+
+        // if(Input.GetKeyDown("Fire1")){
+        //     Debug.Log("hit");
+            // if(col.GetComponent<EnemyStats>()){
+            //     EnemyStats stats = col.GetComponent<EnemyStats>();
+            //     stats.Hit(damage);
+            // }
+        // }
+    }
+}
---
+++ Assets/Scripts/Legolas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legolas : MonoBehaviour
+{
+    float _charge;
+
+    public float chargeMax;
+    public float chargeRate;
+    string fireButton = "Fire1";
+
+    public Transform spawn;
+    public Rigidbody arrowObj;
+
+    void Update(){
+        if(Input.GetButton(fireButton.ToString()) && _charge < chargeMax){
+            Debug.Log("DOWN");
+
+            _charge += Time.deltaTime * chargeRate;
+            Debug.Log(_charge.ToString());
+        }
+
+        if(Input.GetButtonUp(fireButton.ToString())){
+            Debug.Log(_charge.ToString());
+            Rigidbody arrow = Instantiate(arrowObj, spawn.position, Quaternion.identity) as Rigidbody;
+            arrow.AddForce(spawn.forward * _charge, ForceMode.Impulse);
+            _charge = 0;
+            Debug.Log("UP");
+        }
+    }
+}
